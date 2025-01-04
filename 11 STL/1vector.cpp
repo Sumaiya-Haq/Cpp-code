d421ce0054d5c557a97b77419921570782ab2de4
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-01-04 12:52:36 +0600

    multiple vector elements creating

---
+++ 11 STL/1vector.cpp
@@ -0,0 +1,38 @@
+//1st types...
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+      // Creating a vector of 5 elements
+      vector<int> v = {1, 4, 2, 3, 5};
+
+      for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+      return 0;
+}
+
+
+
+//2nd types ...
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector <int>variable ;  //syntex of vector.
+    variable.push_back(5);  //push_back use because we put elements.
+    variable.push_back(55);
+    variable.push_back(3);
+    variable.push_back(4);
+    variable.push_back(24);
+    variable.push_back(15);
+
+    for(int i=0;i<variable.size();i++)
+    {
+    cout<<variable[i]<<endl;
+    
+    
+   }
+ 
+    return 0;
+}
