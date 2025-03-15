89e5a67f2221e4bb89400f1aba7e78a68a74fff2
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-03-15 23:41:51 +0600

    Remove Duplicates in Array

---
+++ 2 array/22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 2, 9, 2, 7, 9, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + size);
+
+    int newSize = 1;  
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] != arr[i - 1]) {
+            arr[newSize] = arr[i];
+            newSize++;
+        }
+    }
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < newSize; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
