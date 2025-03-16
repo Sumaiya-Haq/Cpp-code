a9f6835211e5232de758b66e081e9723c6be5c92
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-03-16 11:33:23 +0600

    deletion

--- 2 array/24.cpp
+++ 2 array/24.cpp
@@ -6,3 +6,19 @@ int a,b,c;
 cout <<"Enter the size of an array: ";
 cin>>a;
 int A[a];
+cout <<" Enter the element of this array: ";
+for (int i=0;i<a;i++){
+    cin >> A[i];
+}
+cout << "Enter the position where you delate an element: ";
+cin >>b;
+
+for (int i=b-1 ; i<a ;i++){
+    A[i]=A[i+1];
+    
+}
+a--;
+for (int i= 0; i<a ;i++){
+    cout<<A[i]<<",";
+}
+}
