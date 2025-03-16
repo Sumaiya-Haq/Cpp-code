1a946ba41eefaaef18ebaf49084c17f84e28983a
Sumaiya-Haq <163652836+Sumaiya-Haq@users.noreply.github.com>
2025-03-16 11:34:41 +0600

    travers and rivers

--- 2 array/25.cpp
+++ 2 array/25.cpp
@@ -9,3 +9,23 @@ int main(){
     for (int i=0;i<a;i++){
         cin >> A[i];
     }
+    //trivarse
+    cout <<"triverse: ";
+    for (int i=0;i<a;i++){
+        cout <<A[i];
+    }
+    cout<<endl;
+//rivers
+for (int i=0;i<a/2;i++){
+    // for(int j=a-1;j>=0;j--){
+        c=A[i];
+        A[i]=A[a-i-1];
+        A[a-i-1]=c;
+    
+}
+cout <<"riverse: ";
+for (int i=0;i<a;i++){
+    cout <<A[i];
+}
+
+}
